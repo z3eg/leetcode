a024cbcfd4cecac2dcc5bc933f642823d41fe882
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2022-07-25 15:23:17 -0400

    _39_CombinationSum_5_5

--- src/main/java/com/company/_509_FibonacciNumber.java
+++ src/main/java/com/company/_509_FibonacciNumber.java
@@ -30,7 +30,7 @@ public class _509_FibonacciNumber {
         assertEquals(6765, fib(20));
     }
 
-    /*The Fibonacci numbers, commonly denotedda F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+    /*The Fibonacci numbers, commonly denoteddajg F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
 
     F(0) = 0, F(1) = 1
     F(n) = F(n - 1) + F(n - 2), for n > 1.
