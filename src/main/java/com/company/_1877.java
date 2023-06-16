19fa9bdf320e62b3298fac41bc8974fc33382f89
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2023-06-15 20:21:36 -0400

    1877

---
+++ src/main/java/com/company/_1877.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class _1877 {
+    /*beats 12.3%-42.39%*/
+    public int minPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length / 2; i++) {
+            max = Math.max(max, nums[i] + nums[nums.length-1-i]);
+        }
+        return max;
+    }
+}
