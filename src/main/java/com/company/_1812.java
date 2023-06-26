d5986a95eb56856705b78b9870ef788d7a974e49
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2023-06-26 16:07:36 -0400

    formatting

--- src/main/java/com/company/_1812.java
+++ src/main/java/com/company/_1812.java
@@ -6,6 +6,6 @@ import org.junit.Test;
         https://leetcode.com/problems/determine-color-of-a-chessboard-square/*/
 public class _1812 {
     public boolean squareIsWhite(String coordinates) {
-        return (coordinates.charAt(0) + coordinates.charAt(1))%2!=0;
+        return (coordinates.charAt(0) + coordinates.charAt(1))%2 != 0;
     }
 }
