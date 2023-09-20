0ebb0decc003249e4ff1f1aeb91848502df9d9f2
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2023-09-20 19:52:08 -0400

    sliding window

--- src/main/java/com/company/_1658.java
+++ src/main/java/com/company/_1658.java
@@ -119,32 +119,7 @@ public class _1658 {
         return (min==Integer.MAX_VALUE)?-1:min;
     }*/
 
-    public int minOperations(int[] nums, int x) {
-        int[] l = new int[nums.length+1];
-        int[] r = new int[nums.length+1];
-        l[0] = 0;
-        r[0] = 0;
-        for (int i = 1; i < l.length; i++) {
-            l[i] = nums[i-1]+l[i-1];
-            r[i] = nums[r.length-i-1]+r[i-1];
-        }
-        int min = Integer.MAX_VALUE;
-        for (int i = 0; i < l.length; i++) {
-            int rest = x - l[i];
-            int restPos = bs(r, rest,0,r.length-i-1);
-            if (restPos!=-1) {
-                int curRes = i + restPos;
-                min = Math.min(min, curRes);
-            }
-            rest = x - r[i];
-            restPos = bs(l, rest,0,l.length-i-1);
-            if (restPos!=-1) {
-                int curRes = i + restPos;
-                min = Math.min(min, curRes);
-            }
-        }
-        return (min==Integer.MAX_VALUE)?-1:min;
-    }
+
 
     int bs(int[] arr, int n) {
         return bs(arr, n, 0, arr.length);
@@ -175,13 +150,46 @@ public class _1658 {
         assertEquals(-1, bs(new int[]{1,10,17,56,88,97,123}, 11));
     }
 
+    public int minOperations(int[] nums, int x) {
+        int l = 0;
+        int r = 0;
+        int min = Integer.MAX_VALUE;
+        int curSum = 0;
+        int totalSum = 0;
+        for (int n : nums)
+            totalSum+=n;
+        if (totalSum < x)
+            return -1;
+        if (totalSum==x)
+            return nums.length;
+        int rem = totalSum - x;
+        while (r<nums.length && l < nums.length) {
+            while (r<nums.length && curSum < rem) {
+                curSum+=nums[r];
+                r++;
+            }
+            while (r<nums.length && curSum==rem) {
+                min=Math.min(min, l + nums.length-r);
+                curSum+=nums[r];
+                r++;
+            }
+            while (l < nums.length && curSum > rem) {
+                curSum-=nums[l];
+                l++;
+            }
+        }
+        if (curSum==rem)
+            min=Math.min(min, l + nums.length-r);
+        return (min==Integer.MAX_VALUE)?-1:min;
+    }
+
     @Test
     public void test() {
+        assertEquals(5, minOperations(new int[]{1,1,1,1,1,10}, 5));
         assertEquals(3, minOperations(new int[]{1000,1,1,2,3}, 1004));
         assertEquals(16, minOperations(new int[]{8828,9581,49,9818,9974,9869,9991,10000,10000,10000,9999,9993,9904,8819,1231,6309}, 134365));
         assertEquals(-1, minOperations(new int[]{1,1}, 3));
         assertEquals(5, minOperations(new int[]{10,1,1,1,1,1}, 5));
-        assertEquals(5, minOperations(new int[]{1,1,1,1,1,10}, 5));
         assertEquals(5, minOperations(new int[]{3,2,20,1,1,3}, 10));
         assertEquals(-1, minOperations(new int[]{1241,8769,9151,3211,2314,8007,3713,5835,2176,8227,5251,9229,904,1899,5513,7878,8663,3804,2685,3501,1204,9742,2578,8849,1120,4687,5902,9929,6769,8171,5150,1343,9619,3973,3273,6427,47,8701,2741,7402,1412,2223,8152,805,6726,9128,2794,7137,6725,4279,7200,5582,9583,7443,6573,7221,1423,4859,2608,3772,7437,2581,975,3893,9172,3,3113,2978,9300,6029,4958,229,4630,653,1421,5512,5392,7287,8643,4495,2640,8047,7268,3878,6010,8070,7560,8931,76,6502,5952,4871,5986,4935,3015,8263,7497,8153,384,1136}, 894887480));
         assertEquals(-1, minOperations(new int[]{5,6,7,8,9}, 4));
