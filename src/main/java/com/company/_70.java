21de553a0e2c0350d83ebb4e7289a8853336fe18
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2023-10-21 22:45:46 -0400

    70 DP

--- src/main/java/com/company/_70.java
+++ src/main/java/com/company/_70.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.assertEquals;
 public class _70 {
     /*Time Limit Exceeded
     21 / 45 testcases passed*/
-    public int climbStairs(int n) {
+    /*public int climbStairs(int n) {
         return climb(n, 0, 0);
     }
 
@@ -20,10 +20,43 @@ public class _70 {
         res = climb(n, cur+1, res);
         res = climb(n, cur+2, res);
         return res;
+    }*/
+/*
+    public int climbStairs(int n) {
+        int dp[] = new int[45];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 3;
+        return dp(n, 0, 0, dp);
+    }
+
+    int dp(int n, int[] dp) {
+        if (dp[n] != 0)
+            return dp[n];
+
+        return res;
+    }*/
+
+    /*0ms
+    Beats 100.00%of users with Java*/
+    public int climbStairs(int n) {
+        int[] dp = new int[46];
+        dp[1] = 1;
+        dp[2] = 2;
+        return climb(n, dp);
+    }
+
+    int climb(int n, int[] dp) {
+        if (dp[n]!=0)
+            return dp[n];
+        dp[n] = climb(n-1, dp) + climb(n-2, dp);
+        return dp[n];
     }
 
     @Test
     public void test() {
+        climbStairs(45);
         assertEquals(1, climbStairs(1));
         assertEquals(2, climbStairs(2));
         assertEquals(3, climbStairs(3));
