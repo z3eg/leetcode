b17fd6a327b868415bfb0a229503920d30e04872
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2023-11-02 13:34:22 -0400

    2265

--- src/main/java/com/company/_2265_.java
+++ src/main/java/com/company/_2265_.java
@@ -10,7 +10,7 @@ import static org.junit.Assert.assertEquals;
 //Count Nodes Equal to Average of Subtree
 public class _2265_ {
 
-    public int averageOfSubtree(TreeNode root) {
+    /*public int averageOfSubtree(TreeNode root) {
         int res = 0;
         if (root.left!=null) {
             res+=averageOfSubtree(root.left);
@@ -36,6 +36,34 @@ public class _2265_ {
             num++;
         }
         return (int) Math.floor((double) sum/num);
+    }*/
+
+//    1ms
+//    Beats 48.09%of users with Java
+    public int averageOfSubtree(TreeNode root) {
+        int[] res = new int[1];
+        avg(root, res);
+        return res[0];
+    }
+
+    int[] avg(TreeNode root, int[] res) {
+        int[] countAndSum = new int[2];
+        countAndSum[0] = 1;
+        countAndSum[1] = root.val;
+        if (root.left!=null) {
+            int[] leftCAS = avg(root.left, res);
+            countAndSum[0] += leftCAS[0];
+            countAndSum[1] += leftCAS[1];
+        }
+        if (root.right!=null) {
+            int[] rightCAS = avg(root.right, res);
+            countAndSum[0] += rightCAS[0];
+            countAndSum[1] += rightCAS[1];
+        }
+        double avg = Math.floor((double) countAndSum[1] /countAndSum[0]);
+        if (root.val == avg)
+            res[0]++;
+        return countAndSum;
     }
 
 
