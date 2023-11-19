985dc0ad9105184d597cd38ee10a40a4e35f5c90
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2023-11-19 11:10:06 -0500

    1887 81%

--- src/main/java/com/company/_1887.java
+++ src/main/java/com/company/_1887.java
@@ -14,7 +14,35 @@ public class _1887 {
             Beats
 47.37%
     of users with Java*/
+    /*public int reductionOperations(int[] nums) {
+        int[] freqs = new int[50001];
+        int min = Integer.MAX_VALUE;
+        Set<Integer> set = new HashSet<>();
+        for (int n : nums) {
+            freqs[n]++;
+            min = Math.min(min, n);
+            set.add(n);
+        }
+        int res = 0;
+        int tiers = set.size();
+        tiers--;
+        for (int i = 50000; i > min; i--) {
+            if (freqs[i]!=0) {
+                res+=freqs[i]*tiers;
+                tiers--;
+            }
+        }
+        return res;
+    }*/
+
+    /*34
+    ms
+            Beats
+81.58%
+    of users with Java*/
     public int reductionOperations(int[] nums) {
+        if (nums.length==1)
+            return 0;
         int[] freqs = new int[50001];
         int min = Integer.MAX_VALUE;
         Set<Integer> set = new HashSet<>();
