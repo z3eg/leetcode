02f4f4306d156c58169603ec6a83afee0ec53c93
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2023-11-19 22:09:14 -0500

    112 100%

--- src/main/java/com/company/_112.java
+++ src/main/java/com/company/_112.java
@@ -6,7 +6,7 @@ public class _112 {
 
     /*Wrong Answer
     105 / 117 testcases passed*/
-    public boolean hasPathSum(TreeNode root, int targetSum) {
+    /*public boolean hasPathSum(TreeNode root, int targetSum) {
         if (root == null)
             return false;
         return hasPath(root, targetSum, 0);
@@ -18,6 +18,28 @@ public class _112 {
         else {
             return (hasPath(root.left, targetSum, curSum+root.val) ||
                     hasPath(root.right, targetSum, curSum+root.val));
+        }
+    }*/
+
+    /*0 ms
+    Beats
+    100.00%
+    of users with Java*/
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        return hasPath(root, targetSum, 0);
+    }
+
+    boolean hasPath(TreeNode root, int targetSum, int curSum) {
+        if (root == null)
+            return false;
+        else {
+            curSum+=root.val;
+            if (root.left == null && root.right == null)
+                return targetSum == curSum;
+            else {
+                return hasPath(root.left, targetSum, curSum) ||
+                        hasPath(root.right, targetSum, curSum);
+            }
         }
     }
 
