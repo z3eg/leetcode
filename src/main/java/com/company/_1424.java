77668017e3cc1bc56cb6eeb46975f818f8fff471
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2023-11-21 20:48:46 -0500

    1424 55/56 WA

--- src/main/java/com/company/_1424.java
+++ src/main/java/com/company/_1424.java
@@ -1,7 +1,9 @@
 package com.company;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 public class _1424 {
 
@@ -35,7 +37,7 @@ public class _1424 {
 
     /*Time Limit Exceeded
     53 / 56 testcases passed*/
-    public int[] findDiagonalOrder(List<List<Integer>> nums) {
+    /*public int[] findDiagonalOrder(List<List<Integer>> nums) {
         int max = 0;
         List<Integer> resList = new LinkedList<>();
         int numSize = nums.size();
@@ -60,5 +62,34 @@ public class _1424 {
             res[i] = resList.get(i);
         }
         return res;
+    }*/
+
+        /*Wrong Answer
+    55 / 56 testcases passed*/
+    public int[] findDiagonalOrder(List<List<Integer>> nums) {
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        int arrLen = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            List<Integer> list = nums.get(i);
+            arrLen+= list.size();
+            for (int j = 0; j < list.size(); j++) {
+                List<Integer> val = map.get(i + j);
+                if (val==null) {
+                    val = new LinkedList<>();
+                }
+                val.add(list.get(j));
+                map.put(i+j, val);
+            }
+        }
+        int[] arr = new int[arrLen];
+        int index = 0;
+        for (Map.Entry<Integer, List<Integer>> entry : map.entrySet()) {
+            List<Integer> value = entry.getValue();
+            for (int i = value.size()-1; i >= 0; i--) {
+                arr[index] = value.get(i);
+                index++;
+            }
+        }
+        return arr;
     }
 }
