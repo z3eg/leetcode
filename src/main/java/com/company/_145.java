8e22ce15b77b7b46df8623d14fe92202ba888d77
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2023-11-23 19:52:07 -0500

    145 33/68 WA

---
+++ src/main/java/com/company/_145.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import com.company.util.tree.bst.TreeNode;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class _145 {
+
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> res = new LinkedList<>();
+        traverse(root, res);
+        return res;
+    }
+
+    /*Wrong Answer
+33 / 68 testcases passed*/
+    void traverse(TreeNode root, List<Integer> res) {
+        if (root!=null) {
+            traverse(root.right, res);
+            traverse(root.left, res);
+            res.add(root.val);
+        }
+    }
+}
