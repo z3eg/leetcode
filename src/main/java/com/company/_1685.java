be062b8931bfd2c7157b4f9820f61ee29a8dfc26
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2023-11-24 19:33:38 -0500

    1685 99%

--- src/main/java/com/company/_1685.java
+++ src/main/java/com/company/_1685.java
@@ -8,7 +8,7 @@ public class _1685 {
     /*Time Limit Exceeded
 53 / 59 testcases passed
 */
-    public int[] getSumAbsoluteDifferences(int[] nums) {
+    /*public int[] getSumAbsoluteDifferences(int[] nums) {
         int[] res = new int[nums.length];
         for (int i = 0; i < nums.length; i++) {
             int curSum = 0;
@@ -16,6 +16,26 @@ public class _1685 {
                 curSum += Math.abs(nums[i]-nums[j]);
             }
             res[i] = curSum;
+        }
+        return res;
+    }*/
+
+    /*3
+    ms
+    Beats
+    99.36%
+    of users with Java*/
+    public int[] getSumAbsoluteDifferences(int[] nums) {
+        int[] res = new int[nums.length];
+        int totSum = 0;
+        for (int n : nums)
+            totSum+=n;
+        int rSum = totSum;
+        int lSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            res[i] = rSum-(nums.length-i)*nums[i] + i*nums[i]-lSum;
+            rSum-=nums[i];
+            lSum+=nums[i];
         }
         return res;
     }
