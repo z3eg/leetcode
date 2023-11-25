aec85d685859092f4ee344c4008b148d27f826ee
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2023-11-24 19:18:21 -0500

    1685 53/59 TLE

---
+++ src/main/java/com/company/_1685.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+public class _1685 {
+    /*Time Limit Exceeded
+53 / 59 testcases passed
+*/
+    public int[] getSumAbsoluteDifferences(int[] nums) {
+        int[] res = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            int curSum = 0;
+            for (int j = 0; j < nums.length; j++) {
+                curSum += Math.abs(nums[i]-nums[j]);
+            }
+            res[i] = curSum;
+        }
+        return res;
+    }
+
+    @Test
+    public void test() {
+        assertArrayEquals(new int[]{4,3,5}, getSumAbsoluteDifferences(new int[]{2,3,5}));
+        assertArrayEquals(new int[]{24,15,13,15,21}, getSumAbsoluteDifferences(new int[]{1,4,6,8,10}));
+//        assertArrayEquals(new int[]{}, getSumAbsoluteDifferences(new int[]{}));
+    }
+}
