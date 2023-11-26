b3b8ba120009c0af82241869d594925d2eb92d53
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2023-11-26 12:54:54 -0500

    1727 63.86 yup i almost understood it

--- src/main/java/com/company/_1727.java
+++ src/main/java/com/company/_1727.java
@@ -42,7 +42,7 @@ public class _1727 {
     }*/
 
     //NOPE, THIS IS ALSO SHIT
-    public int largestSubmatrix(int[][] matrix) {
+    /*public int largestSubmatrix(int[][] matrix) {
         int[][] onesBefore = new int[matrix.length][matrix[0].length];
         int[][] countOfNs = new int[matrix.length][matrix.length+1]; //each cell represents how many columns that have (col_idx) 1s before them are in this row
         for (int i = 0; i < matrix[0].length; i++) {
@@ -59,6 +59,30 @@ public class _1727 {
         for (int i = 0; i < countOfNs.length; i++) {
             for (int j = 0; j < countOfNs[i].length; j++) {
                 res = Math.max(res, countOfNs[i][j]*j);
+            }
+        }
+        return res;
+    }*/
+
+
+    /*13
+    ms
+            Beats
+63.86%
+    of users with Java*/
+    public int largestSubmatrix(int[][] matrix) {
+        int[][] onesBefore = new int[matrix.length][matrix[0].length];
+        System.arraycopy(matrix[0], 0, onesBefore[0], 0, matrix[0].length);
+        for (int c = 0; c < matrix[0].length; c++) {
+            for (int r = 1; r < matrix.length; r++) {
+                onesBefore[r][c] = matrix[r-1][c]==0?matrix[r][c]:(matrix[r][c]==0?0:onesBefore[r-1][c]+1);
+            }
+        }
+        int res = 0;
+        for (int i = 0; i < onesBefore.length; i++) {
+            Arrays.sort(onesBefore[i]);
+            for(int j=1; j<=onesBefore[0].length; j++){
+                res = Math.max(res, j*onesBefore[i][onesBefore[0].length-j]); //i have literally no idea why we do this tbh
             }
         }
         return res;
