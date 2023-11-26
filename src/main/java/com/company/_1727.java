75bc40e05ea95fe94f60b86edff4c171a3a5b817
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2023-11-26 12:34:44 -0500

    1727 COMMITTING SHIT CAUSE WHY NOT

--- src/main/java/com/company/_1727.java
+++ src/main/java/com/company/_1727.java
@@ -10,7 +10,7 @@ public class _1727 {
 
     /*Wrong Answer
     16 / 58 testcases passed*/
-    public int largestSubmatrix(int[][] matrix) {
+    /*public int largestSubmatrix(int[][] matrix) {
         int[][] onesBefore = new int[matrix.length][matrix[0].length];
         int[] minNonZeroes = new int[matrix.length];
         Arrays.fill(minNonZeroes, Integer.MAX_VALUE);
@@ -39,10 +39,36 @@ public class _1727 {
             res = Math.max(res, minNonZeroes[i]*countNonZeroes[i]);
         }
         return res;
+    }*/
+
+    //NOPE, THIS IS ALSO SHIT
+    public int largestSubmatrix(int[][] matrix) {
+        int[][] onesBefore = new int[matrix.length][matrix[0].length];
+        int[][] countOfNs = new int[matrix.length][matrix.length+1]; //each cell represents how many columns that have (col_idx) 1s before them are in this row
+        for (int i = 0; i < matrix[0].length; i++) {
+            onesBefore[0][i] = matrix[0][i];
+            countOfNs[0][onesBefore[0][i]]++;
+        }
+        for (int c = 0; c < matrix[0].length; c++) {
+            for (int r = 1; r < matrix.length; r++) {
+                onesBefore[r][c] = matrix[r-1][c]==0?matrix[r][c]:(matrix[r][c]==0?0:onesBefore[r-1][c]+1);
+                countOfNs[r][onesBefore[r][c]]++;
+            }
+        }
+        int res = 0;
+        for (int i = 0; i < countOfNs.length; i++) {
+            for (int j = 0; j < countOfNs[i].length; j++) {
+                res = Math.max(res, countOfNs[i][j]*j);
+            }
+        }
+        return res;
     }
 
     @Test
     public void test() {
+        assertEquals(4, largestSubmatrix(new int[][]{{0,0,1},{1,1,1},{1,0,1}}));
+        assertEquals(34, largestSubmatrix(new int[][]{{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,0,1,1},
+                {0,1,1,0,1,1,1,1,0,1,1,0,0,1,0,1,1,1,1,0,1,1,1,1,1,1}})); //TC #16
         assertEquals(0, largestSubmatrix(new int[][]{{0}}));
         assertEquals(1, largestSubmatrix(new int[][]{{1}}));
         assertEquals(2, largestSubmatrix(new int[][]{{0,0},{1,1}}));
@@ -50,7 +76,6 @@ public class _1727 {
         assertEquals(1, largestSubmatrix(new int[][]{{0,0},{0,1}}));
         assertEquals(2, largestSubmatrix(new int[][]{{1,1},{0,1}}));
         assertEquals(4, largestSubmatrix(new int[][]{{1,1},{1,1}}));
-        assertEquals(4, largestSubmatrix(new int[][]{{0,0,1},{1,1,1},{1,0,1}}));
         assertEquals(4, largestSubmatrix(new int[][]{{0,0,1},{1,1,1},{1,0,1}}));
         assertEquals(3, largestSubmatrix(new int[][]{{1,0,1,0,1}}));
         assertEquals(2, largestSubmatrix(new int[][]{{1,1,0},{1,0,1}}));
