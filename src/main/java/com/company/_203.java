1892614b2065ed7dedd37b6163bc96662ce57bd4
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2023-12-01 19:29:35 -0500

    203 92%

--- src/main/java/com/company/_203.java
+++ src/main/java/com/company/_203.java
@@ -6,12 +6,29 @@ public class _203 {
 
     /*Wrong Answer
 50 / 66 testcases passed*/
-    public ListNode removeElements(ListNode head, int val) {
+    /*public ListNode removeElements(ListNode head, int val) {
         while (head!= null && head.val == val)
             head = head.next;
         ListNode headPointer = head;
         while (head!=null && head.next!=null) {
             if (head.next.val==val)
+                head.next = head.next.next;
+            head = head.next;
+        }
+        return headPointer;
+    }*/
+
+    /*1
+    ms
+            Beats
+92.27%
+    of users with Java*/
+    public ListNode removeElements(ListNode head, int val) {
+        while (head!= null && head.val == val)
+            head = head.next;
+        ListNode headPointer = head;
+        while (head!=null && head.next!=null) {
+            while (head.next!= null && head.next.val==val)
                 head.next = head.next.next;
             head = head.next;
         }
