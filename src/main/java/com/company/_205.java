820c0009990918d86664a349ba6d067d0f2aa6d0
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2023-12-02 20:30:27 -0500

    205 25/44 WA

--- src/main/java/com/company/_205.java
+++ src/main/java/com/company/_205.java
@@ -2,12 +2,24 @@ package com.company;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class _205 {
+    /*Wrong Answer
+24 / 44 testcases passed*/
     public boolean isIsomorphic(String s, String t) {
-
+        int[] sFreqs = new int[26];
+        int[] tFreqs = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            sFreqs[s.charAt(i)-'a']++;
+            tFreqs[t.charAt(i)-'a']++;
+        }
+        Arrays.sort(sFreqs);
+        Arrays.sort(tFreqs);
+        return Arrays.equals(sFreqs,tFreqs);
     }
 
     @Test
