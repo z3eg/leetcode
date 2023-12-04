5c70f59793751c24b11e7237df555653757378de
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2023-12-03 19:58:07 -0500

    219 18/58 RE

--- src/main/java/com/company/_219.java
+++ src/main/java/com/company/_219.java
@@ -12,7 +12,29 @@ public class _219 {
 
     /*Wrong Answer
 16 / 58 testcases passed*/
+    /*public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < k; i++) {
+            if (set.contains(nums[i]))
+                return true;
+            set.add(nums[i]);
+        }
+        for (int i = k; i < nums.length; i++) {
+            if (set.contains(nums[i])) {
+                return true;
+            }
+            set.remove(nums[i-k]);
+            set.add(nums[i]);
+        }
+        return false;
+    }*/
+
+
+    /*Runtime Error
+18 / 58 testcases passed*/
     public boolean containsNearbyDuplicate(int[] nums, int k) {
+        if (k==0)
+            return false;
         Set<Integer> set = new HashSet<>();
         for (int i = 0; i < k; i++) {
             if (set.contains(nums[i]))
