5c1b9cea49e92c54e052aa534cfa862cd0875c9d
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2023-12-03 19:55:41 -0500

    219 16/58 WA

--- src/main/java/com/company/_219.java
+++ src/main/java/com/company/_219.java
@@ -2,12 +2,31 @@ package com.company;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class _219 {
-    public boolean containsNearbyDuplicate(int[] nums, int k) {
 
+    /*Wrong Answer
+16 / 58 testcases passed*/
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < k; i++) {
+            if (set.contains(nums[i]))
+                return true;
+            set.add(nums[i]);
+        }
+        for (int i = k; i < nums.length; i++) {
+            if (set.contains(nums[i])) {
+                return true;
+            }
+            set.remove(nums[i-k]);
+            set.add(nums[i]);
+        }
+        return false;
     }
 
     @Test
