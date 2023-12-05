a9aa0ff45615c73218c5f5b08f9d8de64dd6247b
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2023-12-04 21:08:21 -0500

    257 206/208 WA

--- src/main/java/com/company/_257.java
+++ src/main/java/com/company/_257.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import com.company.util.tree.bst.TreeNode;
+import org.junit.jupiter.api.Test;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -12,7 +13,7 @@ public class _257 {
             Beats
 7.84%
     of users with Java*/
-    public List<String> binaryTreePaths(TreeNode root) {
+    /*public List<String> binaryTreePaths(TreeNode root) {
         List<String> res = new LinkedList<>();
         binaryTreePaths(root, new LinkedList<>(), res);
         return res;
@@ -34,5 +35,38 @@ public class _257 {
             binaryTreePaths(root.right, cur, res);
             cur.remove(cur.size()-1);
         }
+    }*/
+
+
+    /*Wrong Answer
+206 / 208 testcases passed*/
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> res = new LinkedList<>();
+        binaryTreePaths(root, new StringBuilder(), res);
+        return res;
+    }
+
+    public void binaryTreePaths(TreeNode root, StringBuilder sb, List<String> res) {
+        if (root!=null) {
+            if (sb.length()>0)
+                sb.append("->");
+            sb.append(root.val);
+            if (root.left==null && root.right==null) {
+                res.add(sb.toString());
+            }
+            binaryTreePaths(root.left, sb, res);
+            binaryTreePaths(root.right, sb, res);
+            if (sb.length()>2)
+                sb.delete(sb.length()-3, sb.length());
+        }
+    }
+
+    @Test
+    public void test() {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.right = new TreeNode(5);
+        List<String> strings = binaryTreePaths(root);
     }
 }
