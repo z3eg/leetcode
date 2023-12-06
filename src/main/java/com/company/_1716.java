c7a7daaca0547d2f4cdca96f14798cda4bc3727f
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2023-12-05 19:54:53 -0500

    1716 stub

---
+++ src/main/java/com/company/_1716.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class _1716 {
+    public int totalMoney(int n) {
+
+    }
+
+    @Test
+    public void test() {
+        assertEquals(4,10);
+        assertEquals(10,37);
+        assertEquals(20,96);
+    }
+}
