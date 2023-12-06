1f714b021183f0c50694d43bcfbd03ea7386ec3c
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2023-12-05 20:10:04 -0500

    1716 100%

--- src/main/java/com/company/_1716.java
+++ src/main/java/com/company/_1716.java
@@ -5,14 +5,23 @@ import org.junit.jupiter.api.Test;
 import static org.junit.Assert.assertEquals;
 
 public class _1716 {
-    public int totalMoney(int n) {
 
+    /*0
+    ms
+            Beats
+100.00%
+    of users with Java*/
+    public int totalMoney(int n) {
+        int weeks = n/7;
+        n%=7;
+        n+=weeks;
+        return weeks*28+(((weeks*(weeks-1))/2)*7)+((n*(n+1))/2)-((weeks*(weeks+1))/2);
     }
 
     @Test
     public void test() {
-        assertEquals(4,10);
-        assertEquals(10,37);
-        assertEquals(20,96);
+        assertEquals(96,totalMoney(20));
+        assertEquals(10,totalMoney(4));
+        assertEquals(37,totalMoney(10));
     }
 }
