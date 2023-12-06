5e126067b944e24d72f766091d3fd06bd350efa7
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2023-12-05 20:27:14 -0500

    263 423/1013 WA

--- src/main/java/com/company/_263.java
+++ src/main/java/com/company/_263.java
@@ -9,10 +9,31 @@ public class _263 {
 
     /*Wrong Answer
 428 / 1013 testcases passed*/
-    public boolean isUgly(int n) {
+    /*public boolean isUgly(int n) {
         for (int i = 2; i < n; i++) {
             if (n%i==0 && (n/i!=3 && n/i!=2 && n/i!=5))
                     return false;
+        }
+        return true;
+    }*/
+
+   /* Wrong Answer
+    423 / 1013 testcases passed*/
+    public boolean isUgly(int n) {
+        for (int i = 2; i < n; i++) {
+            if (n%i==0) {
+                if (isPrime(i))
+                    if (i!=2 && i!=3 && i!=5)
+                        return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean isPrime(int i) {
+        for (int j = 2; j < i; j++) {
+            if (i%j==0)
+                return false;
         }
         return true;
     }
