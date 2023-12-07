f3a0b030457ae49bd5eed782ca586c5c6822e3f9
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2023-12-06 22:49:07 -0500

    278 TLE

--- src/main/java/com/company/_278.java
+++ src/main/java/com/company/_278.java
@@ -7,15 +7,17 @@ public class _278 {
     }
 
     int bs(int l, int r) {
+        if (l==0 && isBadVersion(l))
+            return 0;
         int mid = (r+l)/2;
         if (mid==1 && isBadVersion(mid))
             return 1;
         if (isBadVersion(mid) && !isBadVersion(mid-1))
             return mid;
         if (isBadVersion(mid))
-            return bs(mid, r);
-        if (!isBadVersion(mid))
             return bs(l, mid);
+        if (!isBadVersion(mid))
+            return bs(mid, r);
         return 0;
     }
 
