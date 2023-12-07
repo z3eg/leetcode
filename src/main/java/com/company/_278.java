e092224e61f5b9f4d6483925effddf54172c590f
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2023-12-06 23:04:11 -0500

    278 13/24 WA

--- src/main/java/com/company/_278.java
+++ src/main/java/com/company/_278.java
@@ -6,9 +6,9 @@ public class _278 {
         return bs(1,n);
     }
 
+    /*Wrong Answer
+13 / 24 testcases passed*/
     int bs(int l, int r) {
-        if (l-r < 2)
-            return (isBadVersion(l)?l:r);
         if (l==1 && isBadVersion(l))
             return 1;
         int mid = (r+l)/2;
@@ -16,6 +16,10 @@ public class _278 {
             return 1;
         if (isBadVersion(mid) && !isBadVersion(mid-1))
             return mid;
+        if (isBadVersion(mid+1) && !isBadVersion(mid))
+            return mid+1;
+        if (l-r < 2)
+            return (isBadVersion(l)?l:isBadVersion(r)?r:r+1);
         if (isBadVersion(mid))
             return bs(l, mid);
         if (!isBadVersion(mid))
