8d89f6536102fa05ed58a4a3e0f18bff867b3a9c
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2023-12-11 22:38:42 -0500

    367 100%

---
+++ src/main/java/com/company/_367.java
@@ -0,0 +1,49 @@
+package com.company;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class _367 {
+
+    /*0
+    ms
+            Beats
+100.00%
+    of users with Java*/
+    public boolean isPerfectSquare(int num) {
+        return bs(1,46340, num) != -1;
+    }
+
+    int bs(int l, int r, int num) {
+        if (l*l == num)
+            return l;
+        if (r*r == num)
+            return r;
+        if (r-l < 2) {
+            return -1;
+        }
+        int mid = (l+r)/2;
+        int midSquare = mid * mid;
+        if (midSquare == num)
+            return mid;
+        else if (midSquare > num) {
+            return bs(l, mid, num);
+        }
+        else  {
+            return bs(mid, r, num);
+        }
+    }
+
+    @Test
+    public void test() {
+        assertTrue(isPerfectSquare(1));
+        assertTrue(isPerfectSquare(4));
+        assertTrue(isPerfectSquare(9));
+        assertTrue(isPerfectSquare(16));
+        assertTrue(isPerfectSquare(2147395600));
+        assertFalse(isPerfectSquare(2147483647));
+        assertFalse(isPerfectSquare(14));
+    }
+}
