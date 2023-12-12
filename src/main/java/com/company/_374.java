21185c26fff4a68bc8e72b004b1b077044775de7
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2023-12-11 22:57:55 -0500

    374 100%

--- src/main/java/com/company/_374.java
+++ src/main/java/com/company/_374.java
@@ -2,18 +2,38 @@ package com.company;
 
 public class _374 {
 
-    /*Runtime Error
-14 / 25 testcases passed*/
     public int guessNumber(int n) {
         return bs(1,n);
     }
 
-    int bs(int l, int r) {
+    /*Runtime Error
+14 / 25 testcases passed*/
+    /*int bs(int l, int r) {
         if (guess(l)==0)
             return l;
         if (guess(r)==0)
             return r;
         int mid = (l+r)/2;
+        int res = guess(mid);
+        if (res == 0)
+            return mid;
+        else if (res == -1)
+            return bs(l,mid);
+        else
+            return bs(mid,r);
+    }*/
+
+    /*0
+    ms
+            Beats
+100.00%
+    of users with Java*/
+    int bs(int l, int r) {
+        if (guess(l)==0)
+            return l;
+        if (guess(r)==0)
+            return r;
+        int mid = l + (r-l)/2;
         int res = guess(mid);
         if (res == 0)
             return mid;
