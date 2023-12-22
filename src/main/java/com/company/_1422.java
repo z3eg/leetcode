3548fd3a4c9a1844f52533a9e832e880de4788e5
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2023-12-21 19:30:34 -0500

    1422 97%

--- src/main/java/com/company/_1422.java
+++ src/main/java/com/company/_1422.java
@@ -8,7 +8,7 @@ public class _1422 {
 
     /*Wrong Answer
 96 / 104 testcases passed*/
-    public int maxScore(String s) {
+    /*public int maxScore(String s) {
         int score = 0;
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i)=='1') {
@@ -17,6 +17,38 @@ public class _1422 {
         }
         int max_score = score-1;
         for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i)=='0') {
+                score++;
+                max_score = Math.max(score, max_score);
+            }
+            else {
+                score--;
+            }
+        }
+        return max_score;
+    }*/
+
+
+    /*1
+    ms
+            Beats
+97.78%
+    of users with Java*/
+    public int maxScore(String s) {
+        int score = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i)=='1') {
+                score++;
+            }
+        }
+        if (s.charAt(0)=='0') {
+            score++;
+        }
+        else {
+            score--;
+        }
+        int max_score = score;
+        for (int i = 1; i < s.length()-1; i++) {
             if (s.charAt(i)=='0') {
                 score++;
                 max_score = Math.max(score, max_score);
