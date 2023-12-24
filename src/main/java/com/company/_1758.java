f243151ecbb96ef04db80d886240c6ed7bbcff77
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2023-12-24 11:16:32 -0500

    1758 81/89 WA

--- src/main/java/com/company/_1758.java
+++ src/main/java/com/company/_1758.java
@@ -5,14 +5,37 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class _1758 {
-    public int minOperations(String s) {
 
+    /*Wrong Answer
+81 / 89 testcases passed*/
+    public int minOperations(String s) {
+        char[] larr = s.toCharArray();
+        char[] rarr = s.toCharArray();
+        int l = 0;
+        int r =0;
+        for (int i = 1; i < larr.length; i++) {
+            if (larr[i]==larr[i-1]) {
+                larr[i] = larr[i]=='1'?'0':'1';
+                l++;
+            }
+        }
+        for (int i = rarr.length-2; i >= 0; i--) {
+            if (rarr[i]==rarr[i+1]) {
+                rarr[i] = rarr[i]=='1'?'0':'1';
+                r++;
+            }
+        }
+        return Math.min(l,r);
     }
 
     @Test
     public void test() {
+        assertEquals(0, minOperations("0"));
+        assertEquals(0, minOperations("1"));
         assertEquals(1, minOperations("0100"));
         assertEquals(0, minOperations("10"));
         assertEquals(2, minOperations("1111"));
+        assertEquals(0, minOperations("01010"));
+        assertEquals(1, minOperations("01110"));
     }
 }
