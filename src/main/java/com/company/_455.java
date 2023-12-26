8364f45c53f54dae5a6b3ad26e3793ce98fc9b1c
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2023-12-26 18:07:39 -0500

    455 12/21 WA

--- src/main/java/com/company/_455.java
+++ src/main/java/com/company/_455.java
@@ -24,7 +24,7 @@ public class _455 {
 
     /*Wrong Answer
 11 / 21 testcases passed*/
-    public int findContentChildren(int[] g, int[] s) {
+    /*public int findContentChildren(int[] g, int[] s) {
         Arrays.sort(g);
         Arrays.sort(s);
         int counter = 0;
@@ -36,11 +36,29 @@ public class _455 {
                 counter++;
         }
         return counter;
+    }*/
+
+    /*Wrong Answer
+12 / 21 testcases passed*/
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int counter = 0;
+        int l = 0;
+        for (int i = 0; i < g.length; i++) {
+            while (l < s.length && g[i]>s[l])
+                l++;
+            if (l < s.length && g[i]>=s[l]) {
+                counter++;
+                l++;
+            }
+        }
+        return counter;
     }
 
     @Test
     public void test() {
-        assertEquals(1, findContentChildren(new int[]{10,9,8,7,10,9,8,7}, new int[]{10,9,8,7}));
+        assertEquals(4, findContentChildren(new int[]{10,9,8,7,10,9,8,7}, new int[]{10,9,8,7}));
         assertEquals(1, findContentChildren(new int[]{1,2,3}, new int[]{1,1}));
         assertEquals(2, findContentChildren(new int[]{1,2}, new int[]{1,2,3}));
     }
