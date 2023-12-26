4b8c25099621e2ea792f5324f2d74dcc3f646b56
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2023-12-25 23:09:48 -0500

    434 21/28 WA

--- src/main/java/com/company/_434.java
+++ src/main/java/com/company/_434.java
@@ -1,10 +1,30 @@
 package com.company;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class _434 {
 
     /*Wrong Answer
 20 / 28 testcases passed*/
-    public int countSegments(String s) {
+    /*public int countSegments(String s) {
         return s.split("\s").length;
+    }*/
+
+    /*Wrong Answer
+21 / 28 testcases passed*/
+    public int countSegments(String s) {
+        if (s.length()==0)
+            return 0;
+        return s.split(" ").length;
+    }
+
+    @Test
+    public void test() {
+        assertEquals(0, countSegments(""));
+        assertEquals(0, countSegments(" "));
+        assertEquals(0, countSegments("  "));
+        assertEquals(0, countSegments("   "));
     }
 }
