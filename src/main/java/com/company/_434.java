4ea0989c40d88ceb372a09a807d4639fbedb761d
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2023-12-25 22:51:09 -0500

    434 20/28 WA

---
+++ src/main/java/com/company/_434.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class _434 {
+
+    /*Wrong Answer
+20 / 28 testcases passed*/
+    public int countSegments(String s) {
+        return s.split("\s").length;
+    }
+}
