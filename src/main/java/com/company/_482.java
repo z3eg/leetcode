a648e725c320f3158c40cbddfd3213d747de3245
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2023-12-26 20:04:08 -0500

    482 26%

--- src/main/java/com/company/_482.java
+++ src/main/java/com/company/_482.java
@@ -7,7 +7,7 @@ public class _482 {
             Beats
 14.11%
     of users with Java*/
-    public String licenseKeyFormatting(String s, int k) {
+    /*public String licenseKeyFormatting(String s, int k) {
         s = s.replaceAll("-","");
         s = s.toUpperCase();
         StringBuilder stringBuilder = new StringBuilder();
@@ -17,5 +17,23 @@ public class _482 {
             stringBuilder.insert(0,s.charAt(s.length()-i));
         }
         return stringBuilder.toString();
+    }*/
+
+    /*65
+    ms
+            Beats
+26.38%
+    of users with Java*/
+    public String licenseKeyFormatting(String s, int k) {
+        s = s.replaceAll("-","");
+        s = s.toUpperCase();
+        StringBuilder sb = new StringBuilder(s);
+        for (int i = 1; i <= s.length()/k; i++) {
+            int offset = sb.length() - (i-1) - i * k;
+            if (offset < 0)
+                break;
+            sb.insert(offset, "-");
+        }
+        return sb.toString();
     }
 }
