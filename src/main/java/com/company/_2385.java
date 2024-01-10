76f66c1a7dd21162c1f26ad0ff1a7aa133e83c94
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2024-01-09 20:54:16 -0500

    2385 56/80 WA

--- src/main/java/com/company/_2385.java
+++ src/main/java/com/company/_2385.java
@@ -11,7 +11,7 @@ public class _2385 {
 
     /*Wrong Answer
 46 / 80 testcases passed*/
-    public int amountOfTime(TreeNode root, int start) {
+    /*public int amountOfTime(TreeNode root, int start) {
         List<List<Integer>> paths = new LinkedList<>();
         bfs(root, new LinkedList<>(), paths, start);
         int max = 0;
@@ -21,6 +21,38 @@ public class _2385 {
             }
             else max = Math.max(max, (path.size()-1) + depthOfStart);
         }
+        return max;
+    }
+
+    private void bfs(TreeNode root, LinkedList<Integer> curPath, List<List<Integer>> paths, int start) {
+        if (root != null) {
+            curPath.add(root.val);
+            if (root.val == start) {
+                depthOfStart = curPath.size()-1;
+            }
+            if (root.right==null && root.left == null) {
+                paths.add(new LinkedList<>(curPath));
+            }
+            else {
+                bfs(root.left, curPath, paths, start);
+                bfs(root.right, curPath, paths, start);
+            }
+            curPath.remove(Integer.valueOf(root.val));
+        }
+    }*/
+
+    /*Wrong Answer
+56 / 80 testcases passed*/
+    public int amountOfTime(TreeNode root, int start) {
+        List<List<Integer>> paths = new LinkedList<>();
+        bfs(root, new LinkedList<>(), paths, start);
+        int max = 0;
+        for (List<Integer> path : paths) {
+            if (path.contains(start))
+                max = Math.max(max, (path.size()-1)-depthOfStart);
+            else max = Math.max(max, (path.size()-1) + depthOfStart);
+        }
+        max = Math.max(max, depthOfStart);
         return max;
     }
 
