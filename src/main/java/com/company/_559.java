b1c215582ac8386eb9e12af59e55abb863b26116
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2024-01-11 21:51:15 -0500

    559 100%

--- src/main/java/com/company/_559.java
+++ src/main/java/com/company/_559.java
@@ -22,7 +22,33 @@ public class _559 {
 
     /*Wrong Answer
 37 / 38 testcases passed*/
+    /*public int maxDepth(Node root) {
+        return maxDep(root, 1, 1);
+    }
+
+    int maxDep(Node root, int curDepth, int curMaxDepth) {
+        if (root!=null) {
+            if (root.children==null || root.children.isEmpty()) {
+                return Math.max(curDepth, curMaxDepth);
+            }
+            else {
+                for (Node child : root.children) {
+                    curMaxDepth = maxDep(child, curDepth+1, curMaxDepth);
+                }
+                return curMaxDepth;
+            }
+        }
+        return curDepth;
+    }*/
+
+    /*0
+    ms
+            Beats
+100.00%
+    of users with Java*/
     public int maxDepth(Node root) {
+        if (root == null)
+            return 0;
         return maxDep(root, 1, 1);
     }
 
