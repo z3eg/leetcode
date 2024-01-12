7a2ca9de2a6f7498b0ffb0338b2f5d06e769c260
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2024-01-11 21:50:00 -0500

    559 37/38 WA

---
+++ src/main/java/com/company/_559.java
@@ -0,0 +1,43 @@
+package com.company;
+
+import java.util.List;
+
+public class _559 {
+
+    class Node {
+        public int val;
+        public List<Node> children;
+
+        public Node() {}
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, List<Node> _children) {
+            val = _val;
+            children = _children;
+        }
+    };
+
+    /*Wrong Answer
+37 / 38 testcases passed*/
+    public int maxDepth(Node root) {
+        return maxDep(root, 1, 1);
+    }
+
+    int maxDep(Node root, int curDepth, int curMaxDepth) {
+        if (root!=null) {
+            if (root.children==null || root.children.isEmpty()) {
+                return Math.max(curDepth, curMaxDepth);
+            }
+            else {
+                for (Node child : root.children) {
+                    curMaxDepth = maxDep(child, curDepth+1, curMaxDepth);
+                }
+                return curMaxDepth;
+            }
+        }
+        return curDepth;
+    }
+}
