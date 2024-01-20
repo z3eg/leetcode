17b808e71494d855f79e55ccaadad67e5ace7b59
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2024-01-20 15:26:25 -0500

    907 WA 73/87

---
+++ src/main/java/com/company/_907.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class _907 {
+
+    /*Wrong Answer
+73 / 87 testcases passed*/
+    public int sumSubarrayMins(int[] arr) {
+        int MODULO = 1_000_000_000;
+        int sum = 0;
+        for (int len = 1; len <= arr.length; len++) {
+            for (int start = 0; start <= arr.length - len; start++) {
+                int min = arr[start];
+                for (int i = start+1; i < start+len; i++) {
+                    min = Math.min(min,arr[i]);
+                }
+                sum+=min;
+            }
+        }
+        return sum%MODULO;
+    }
+
+    @Test
+    public void test() {
+        assertEquals(17, sumSubarrayMins(new int[]{3,1,2,4}));
+        assertEquals(444, sumSubarrayMins(new int[]{11,81,94,43,3}));
+    }
+}
