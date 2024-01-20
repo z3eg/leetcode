e516b15bc648c4001059ff2163b95228f72cbba6
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2024-01-20 15:31:37 -0500

    907 77/87 TLE

--- src/main/java/com/company/_907.java
+++ src/main/java/com/company/_907.java
@@ -8,7 +8,7 @@ public class _907 {
 
     /*Wrong Answer
 73 / 87 testcases passed*/
-    public int sumSubarrayMins(int[] arr) {
+    /*public int sumSubarrayMins(int[] arr) {
         int MODULO = 1_000_000_000;
         int sum = 0;
         for (int len = 1; len <= arr.length; len++) {
@@ -18,6 +18,23 @@ public class _907 {
                     min = Math.min(min,arr[i]);
                 }
                 sum+=min;
+            }
+        }
+        return sum%MODULO;
+    }*/
+
+    /*Time Limit Exceeded
+    77 / 87 testcases passed*/
+    public int sumSubarrayMins(int[] arr) {
+        int MODULO = 1_000_000_007;
+        int sum = 0;
+        for (int len = 1; len <= arr.length; len++) {
+            for (int start = 0; start <= arr.length - len; start++) {
+                int min = arr[start];
+                for (int i = start+1; i < start+len; i++) {
+                    min = Math.min(min,arr[i]);
+                }
+                sum=(sum+min)%MODULO;
             }
         }
         return sum%MODULO;
