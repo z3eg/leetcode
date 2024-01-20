be959d9d59656da0eb6168f2f8a11e8f206447f3
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2024-01-20 15:47:45 -0500

    907 77/87 TLE

--- src/main/java/com/company/_907.java
+++ src/main/java/com/company/_907.java
@@ -1,6 +1,10 @@
 package com.company;
 
 import org.junit.Test;
+import scala.Int;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static junit.framework.TestCase.assertEquals;
 
@@ -25,7 +29,7 @@ public class _907 {
 
     /*Time Limit Exceeded
     77 / 87 testcases passed*/
-    public int sumSubarrayMins(int[] arr) {
+    /*public int sumSubarrayMins(int[] arr) {
         int MODULO = 1_000_000_007;
         int sum = 0;
         for (int len = 1; len <= arr.length; len++) {
@@ -38,7 +42,60 @@ public class _907 {
             }
         }
         return sum%MODULO;
+    }*/
+
+    /*public int sumSubarrayMins(int[] arr) {
+        Map<String, Integer> map = new HashMap<>();
+        int MODULO = 1_000_000_007;
+        int sum = 0;
+        for (int len = 1; len <= arr.length; len++) {
+            for (int start = 0; start <= arr.length - len; start++) {
+                int min;
+                String key = start + "_" + (start + len);
+                if (map.containsKey(key))
+                    min = map.get(key);
+                else {
+                    min = arr[start];
+                    for (int i = start + 1; i < start + len; i++) {
+                        min = Math.min(min, arr[i]);
+                    }
+                    map.put(key,min);
+                }
+                sum=(sum+min)%MODULO;
+            }
+        }
+        return sum%MODULO;
+    }*/
+
+    //TLE
+    public int sumSubarrayMins(int[] arr) {
+        Map<String, Integer> map = new HashMap<>();
+        int MODULO = 1_000_000_007;
+        int sum = 0;
+        for (int len = 1; len <= arr.length; len++) {
+            for (int start = 0; start <= arr.length - len; start++) {
+                int min = min(start, len, map, arr);
+                sum=(sum+min)%MODULO;
+            }
+        }
+        return sum%MODULO;
     }
+
+    int min(int start, int len, Map<String, Integer> map, int[] arr) {
+        int min;
+        String key = start + "_" + (start + len);
+        if (map.containsKey(key))
+            min = map.get(key);
+        else {
+            if (start+1 == arr.length || len==1)
+                min = arr[start];
+            else
+                min = Math.min(arr[start], min(start+1, len-1, map, arr));
+            map.put(key,min);
+        }
+        return min;
+    }
+
 
     @Test
     public void test() {
