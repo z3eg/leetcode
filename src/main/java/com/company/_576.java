eadd9d495edf97082373477ce574b22bb4cdc6c0
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2024-01-26 15:32:05 -0500

    576 30/94 WA

--- src/main/java/com/company/_576.java
+++ src/main/java/com/company/_576.java
@@ -2,11 +2,35 @@ package com.company;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+
 import static org.junit.Assert.assertEquals;
 
 public class _576 {
+
+    /*Wrong Answer
+30 / 94 testcases passed*/
     public int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
-        return 0;
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < n; i++) {
+            dp[0][i]++;
+            dp[dp.length-1][i]++;
+        }
+        for (int i = 0; i < m; i++) {
+            dp[i][0]++;
+            dp[i][dp[0].length-1]++;
+        }
+        return dp(startRow, startColumn, maxMove, dp);
+    }
+
+    private int dp(int r, int c, int movesLeft, int[][] dp) {
+        if (r<0 || r>=dp.length || c < 0 || c >= dp[0].length)
+            return 0;
+        if (movesLeft<=0)
+            return 0;
+        dp[r][c] += dp(r-1,c,movesLeft-1, dp) + dp(r+1,c,movesLeft-1, dp) +
+                dp(r, c-1, movesLeft-1, dp) + dp(r, c+1, movesLeft-1, dp);
+        return dp[r][c];
     }
 
     @Test
