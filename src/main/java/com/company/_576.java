c09cc78221ca2862139336864f7ce09a73ae9853
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2024-01-26 10:02:04 -0500

    576 stub

---
+++ src/main/java/com/company/_576.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class _576 {
+    public int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
+        return 0;
+    }
+
+    @Test
+    public void test() {
+        assertEquals(6, findPaths(2,2,2,0,0));
+        assertEquals(12, findPaths(1,3,3,0,1));
+    }
+}
