83f30524024964b61065fe4659441f60d7497b12
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2024-01-29 16:23:18 -0500

    232 100%

--- src/main/java/com/company/_232.java
+++ src/main/java/com/company/_232.java
@@ -11,7 +11,7 @@ public class _232 {
 
     /*Wrong Answer
 17 / 22 testcases passed*/
-    class MyQueue {
+    /*class MyQueue {
 
         Stack<Integer> stack1;
         Stack<Integer> stack2;
@@ -40,16 +40,65 @@ public class _232 {
         public boolean empty() {
             return stack2.empty();
         }
+    }*/
+
+    /*0
+    ms
+            Beats
+100.00%
+    of users with Java*/
+    class MyQueue {
+
+        Stack<Integer> stack1;
+        Stack<Integer> stack2;
+
+        public MyQueue() {
+            stack1 = new Stack<>();
+            stack2 = new Stack<>();
+        }
+
+        public void push(int x) {
+            while (!stack2.isEmpty())
+                stack1.push(stack2.pop());
+            stack2.push(x);
+            while (!stack1.isEmpty())
+                stack2.push(stack1.pop());
+        }
+
+        public int pop() {
+            return stack2.pop();
+        }
+
+        public int peek() {
+            return stack2.peek();
+        }
+
+        public boolean empty() {
+            return stack2.empty();
+        }
     }
 
     @Test
     public void test() {
         MyQueue myQueue = new MyQueue();
-        myQueue.push(1); // queue is: [1]
+        /*myQueue.push(1); // queue is: [1]
         myQueue.push(2); // queue is: [1, 2] (leftmost is front of the queue)
         assertEquals(1, myQueue.peek()); // return 1
         assertEquals(1,myQueue.pop()); // return 1, queue is [2]
-        assertFalse(myQueue.empty()); // return false
+        assertFalse(myQueue.empty()); // return false*/
+
+
+        myQueue = new MyQueue();
+        myQueue.push(1); // queue is: [1]
+        myQueue.push(2); // queue is: [1, 2] (leftmost is front of the queue)
+        myQueue.push(3); // queue is: [1, 2] (leftmost is front of the queue)
+        myQueue.push(4); // queue is: [1, 2] (leftmost is front of the queue)
+        assertEquals(1, myQueue.pop()); // return 1
+        myQueue.push(5); // queue is: [1, 2] (leftmost is front of the queue)
+        assertEquals(2,myQueue.pop()); // return 1, queue is [2]
+        assertEquals(3,myQueue.pop()); // return 1, queue is [2]
+        assertEquals(4,myQueue.pop()); // return 1, queue is [2]
+        assertEquals(5,myQueue.pop()); // return 1, queue is [2]
     }
 
 }
