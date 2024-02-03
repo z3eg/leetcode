8cf00ffbf4b2d29b3721355452e4bd3010287e8e
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2024-02-02 19:24:30 -0500

    671 25/39 WA

--- src/main/java/com/company/_671.java
+++ src/main/java/com/company/_671.java
@@ -5,14 +5,32 @@ import com.company.util.tree.bst.TreeNode;
 public class _671 {
 
     /*Wrong Answer
-22 / 39 testcases passed*/
-    public int findSecondMinimumValue(TreeNode root) {
+    22 / 39 testcases passed*/
+    /*public int findSecondMinimumValue(TreeNode root) {
         return dfs(root, -1);
     }
 
     int dfs(TreeNode root, int cur) {
         if (root!=null) {
             if (root.left!=null && root.val < root.right.val) {
+                cur = Math.max(cur, root.left.val);
+            }
+            cur = Math.max(cur, dfs(root.left, cur));
+            cur = Math.max(cur, dfs(root.right, cur));
+        }
+        return cur;
+    }*/
+
+    /*Wrong Answer
+25 / 39 testcases passed*/
+    public int findSecondMinimumValue(TreeNode root) {
+        int dfs = dfs(root, root.val);
+        return dfs ==root.val?-1:dfs;
+    }
+
+    int dfs(TreeNode root, int cur) {
+        if (root!=null) {
+            if (root.left!=null) {
                 cur = Math.max(cur, root.left.val);
             }
             cur = Math.max(cur, dfs(root.left, cur));
