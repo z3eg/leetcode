b054dccc2a1856a0282af1be6a52de7312b4d1a4
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2024-02-04 17:49:57 -0500

    680 427/471 TLE

--- src/main/java/com/company/_680.java
+++ src/main/java/com/company/_680.java
@@ -24,7 +24,7 @@ public class _680 {
 
     /*Time Limit Exceeded
 423 / 471 testcases passed*/
-    public boolean validPalindrome(String s) {
+    /*public boolean validPalindrome(String s) {
         if (isPalindrome(s))
             return true;
         for (int i = 0; i < s.length(); i++) {
@@ -40,6 +40,52 @@ public class _680 {
         int l = 0;
         while (l<r) {
             if (s.charAt(l) == s.charAt(r)) {
+                l++;
+                r--;
+            }
+            else {
+                return false;
+            }
+        }
+        return true;
+    }*/
+
+    /*Time Limit Exceeded
+427 / 471 testcases passed*/
+    public boolean validPalindrome(String s) {
+        if (isPalindrome(s))
+            return true;
+        for (int i = 0; i < s.length(); i++) {
+            if (isPalindrome(s, i))
+                return true;
+        }
+        return false;
+    }
+
+    boolean isPalindrome(String s) {
+        int r = s.length()-1;
+        int l = 0;
+        while (l<r) {
+            if (s.charAt(l) == s.charAt(r)) {
+                l++;
+                r--;
+            }
+            else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    boolean isPalindrome(String s, int ignore) {
+        int r = s.length()-1;
+        int l = 0;
+        while (l<r) {
+            if (l==ignore)
+                l++;
+            else  if (r==ignore)
+                r--;
+            else if (s.charAt(l) == s.charAt(r)) {
                 l++;
                 r--;
             }
