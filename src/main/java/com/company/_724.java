65a1ac58986a479df25aef05f9dffdedbe70dd61
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2024-02-09 16:14:11 -0500

    724 315/746 WA

---
+++ src/main/java/com/company/_724.java
@@ -0,0 +1,38 @@
+package com.company;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class _724 {
+
+    /*Wrong Answer
+315 / 746 testcases passed*/
+    public int pivotIndex(int[] nums) {
+        int lSum = 0;
+        int rSum = 0;
+        int l = 0;
+        int r = nums.length-1;
+        while (l<r) {
+            if (lSum >= rSum) {
+                rSum+=nums[r];
+                r--;
+            }
+            else {
+                lSum+=nums[l];
+                l++;
+            }
+        }
+        if (lSum==rSum)
+            return r;
+        else
+            return -1;
+    }
+
+    @Test
+    public void test() {
+        assertEquals(0, pivotIndex(new int[]{2,1,-1}));
+        assertEquals(3, pivotIndex(new int[]{1,7,3,6,5,6}));
+        assertEquals(-1, pivotIndex(new int[]{1,2,3}));
+    }
+}
