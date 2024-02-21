ae4211eeb0632cc6c7fa1d8fb09b25f3559b5790
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2024-02-21 13:57:38 -0500

    783 21%

--- src/main/java/com/company/_783.java
+++ src/main/java/com/company/_783.java
@@ -2,11 +2,15 @@ package com.company;
 
 import com.company.util.tree.bst.TreeNode;
 
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class _783 {
 
     /*Wrong Answer
 31 / 49 testcases passed*/
-    public int minDiffInBST(TreeNode root) {
+    /*public int minDiffInBST(TreeNode root) {
         return dfs(root, Integer.MAX_VALUE);
     }
 
@@ -22,5 +26,34 @@ public class _783 {
             }
         }
         return min;
+    }*/
+
+    /*1
+    ms
+    Beats
+    21.66%
+    of users with Java*/
+    public int minDiffInBST(TreeNode root) {
+        TreeSet<Integer> integers = new TreeSet<>();
+        dfs(root, integers);
+        int min = Integer.MAX_VALUE;
+        Iterator<Integer> iterator = integers.iterator();
+        Integer first = iterator.next();
+        Integer next = iterator.next();
+        while (iterator.hasNext()) {
+            min = Math.min(min, next-first);
+            first = next;
+            next = iterator.next();
+        }
+        min = Math.min(min, next-first);
+        return min;
+    }
+
+    void dfs(TreeNode root, Set<Integer> set) {
+        if (root!=null) {
+            set.add(root.val);
+            dfs(root.left, set);
+            dfs(root.right, set);
+        }
     }
 }
