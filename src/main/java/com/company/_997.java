a779962e13f55b655114fe8c9044d6fe63bb0814
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2024-02-22 14:15:18 -0500

    997 71%

--- src/main/java/com/company/_997.java
+++ src/main/java/com/company/_997.java
@@ -8,7 +8,28 @@ public class _997 {
 
     /*Wrong Answer
 91 / 92 testcases passed*/
+    /*public int findJudge(int n, int[][] trust) {
+        int[] trustGiven = new int[n+1];
+        int[] trustReceived = new int[n+1];
+        for (int[] t : trust) {
+            trustReceived[t[1]]++;
+            trustGiven[t[0]]++;
+        }
+        for (int i = 0; i < n+1; i++) {
+            if (trustReceived[i]==n-1 && trustGiven[i]==0)
+                return i;
+        }
+        return -1;
+    }*/
+
+    /*3
+    ms
+            Beats
+71.04%
+    of users with Java*/
     public int findJudge(int n, int[][] trust) {
+        if (n==1)
+            return 1;
         int[] trustGiven = new int[n+1];
         int[] trustReceived = new int[n+1];
         for (int[] t : trust) {
