5248fbb32f9c8920265b77f17bd8a0ac68e701cb
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2024-02-23 15:15:54 -0500

    796 36/51 WA

--- src/main/java/com/company/_796.java
+++ src/main/java/com/company/_796.java
@@ -9,6 +9,45 @@ public class _796 {
 
     /*Wrong Answer
 31 / 51 testcases passed*/
+    /*class Node {
+        char val;
+        Node next;
+
+        public Node(char val) {
+            this.val = val;
+        }
+    }
+
+    public boolean rotateString(String s, String goal) {
+        Node start = new Node(s.charAt(0));
+        Node first = start;
+        int len = 1;
+        for (int i = 1; i < s.length(); i++) {
+            Node next = new Node(s.charAt(i));
+            first.next = next;
+            first = next;
+            len++;
+        }
+        first.next = start;
+
+        int curLen = 0;
+        while (first.val!=goal.charAt(0) && curLen < len) {
+            curLen++;
+            first = first.next;
+        }
+        if (curLen == len)
+            return false;
+        for (int i = 1; i < goal.length(); i++) {
+            if (goal.charAt(i)!=first.next.val)
+                return false;
+            first = first.next;
+        }
+        return true;
+    }
+*/
+
+    /*Wrong Answer
+    36 / 51 testcases passed*/
     class Node {
         char val;
         Node next;
@@ -19,6 +58,8 @@ public class _796 {
     }
 
     public boolean rotateString(String s, String goal) {
+        if (s.length() != goal.length())
+            return false;
         Node start = new Node(s.charAt(0));
         Node first = start;
         int len = 1;
