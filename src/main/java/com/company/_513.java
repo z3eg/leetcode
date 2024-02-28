1f6eef476ff898ce730d4756b1da679f96388daa
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2024-02-27 19:38:48 -0500

    513 52/77 WA

--- src/main/java/com/company/_513.java
+++ src/main/java/com/company/_513.java
@@ -5,7 +5,7 @@ import com.company.util.tree.bst.TreeNode;
 public class _513 {
     /*Wrong Answer
 49 / 77 testcases passed*/
-    public int findBottomLeftValue(TreeNode root) {
+    /*public int findBottomLeftValue(TreeNode root) {
         int[] res = new int[2];
         dfs(root, 0, res);
         return res[1];
@@ -18,6 +18,28 @@ public class _513 {
                     res[0] = depth+1;
                     res[1] = root.left.val;
                 }
+            }
+            dfs(root.left, depth+1, res);
+            dfs(root.right, depth+1, res);
+        }
+    }*/
+
+    /*Wrong Answer
+52 / 77 testcases passed*/
+    public int findBottomLeftValue(TreeNode root) {
+        if (root.left==null && root.right==null)
+            return root.val;
+        int[] res = new int[2];
+        dfs(root, 0, res);
+        return res[1];
+    }
+
+    void dfs(TreeNode root, int depth, int[] res) {
+        if (root!=null) {
+            if (root.left!=null && (depth+1 > res[0])) {
+                    res[0] = depth+1;
+                    res[1] = root.left.val;
+
             }
             dfs(root.left, depth+1, res);
             dfs(root.right, depth+1, res);
