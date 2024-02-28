c80d7233396a66a66e2bb5f0fb5edaac32b8763a
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2024-02-27 19:40:34 -0500

    513 100%

--- src/main/java/com/company/_513.java
+++ src/main/java/com/company/_513.java
@@ -26,7 +26,7 @@ public class _513 {
 
     /*Wrong Answer
 52 / 77 testcases passed*/
-    public int findBottomLeftValue(TreeNode root) {
+    /*public int findBottomLeftValue(TreeNode root) {
         if (root.left==null && root.right==null)
             return root.val;
         int[] res = new int[2];
@@ -39,6 +39,36 @@ public class _513 {
             if (root.left!=null && (depth+1 > res[0])) {
                     res[0] = depth+1;
                     res[1] = root.left.val;
+
+            }
+            dfs(root.left, depth+1, res);
+            dfs(root.right, depth+1, res);
+        }
+    }*/
+
+    /*0
+    ms
+            Beats
+100.00%
+    of users with Java*/
+    public int findBottomLeftValue(TreeNode root) {
+        if (root.left==null && root.right==null)
+            return root.val;
+        int[] res = new int[2];
+        dfs(root, 0, res);
+        return res[1];
+    }
+
+    void dfs(TreeNode root, int depth, int[] res) {
+        if (root!=null) {
+            if (root.left!=null && (depth+1 > res[0])) {
+                res[0] = depth+1;
+                res[1] = root.left.val;
+
+            }
+            if (root.right!=null && (depth+1 > res[0])) {
+                res[0] = depth+1;
+                res[1] = root.right.val;
 
             }
             dfs(root.left, depth+1, res);
