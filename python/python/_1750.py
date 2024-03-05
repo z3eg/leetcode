c9f9f280d8da208c2baf5f8a9e6496343ec931e3
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2024-03-04 20:26:23 -0500

    1750 fix sonarlint py

--- python/python/_1750.py
+++ python/python/_1750.py
@@ -1,34 +1,35 @@
 import unittest
 
-
-class MyTestCase(unittest.TestCase):
-
 #     71
 # ms
 # Beats
 # 54.03%
 # of users with Python3
-    def minimumLength(self, s: str) -> int:
-        l = 0
-        r = len(str)-1
-        while l < r:
-            if s[l] != s[r]:
-                return r-l+1
-            while l < r and s[l] == s[l+1]:
-                l += 1
-            while l < r and s[r] == s[r-1]:
-                r -= 1
-            l += 1
-            r -= 1
-        diff = r-l
-        if diff >= 0:
-            return diff+1
-        else:
-            return 0
 
+
+def minimum_length(s: str) -> int:
+    left = 0
+    right = len(str)-1
+    while left < right:
+        if s[left] != s[right]:
+            return right-left+1
+        while left < right and s[left] == s[left+1]:
+            left += 1
+        while left < right and s[right] == s[right-1]:
+            right -= 1
+        left += 1
+        right -= 1
+    diff = right-left
+    if diff >= 0:
+        return diff+1
+    else:
+        return 0
+
+
+class MyTestCase(unittest.TestCase):
 
     def test_something(self):
-        self.assertEqual(2, self.minimumLength("ca"))  #TODO figure out unit testing in python
+        self.assertEqual(2, minimum_length("ca"))  # TODO figure out unit testing in python
         self.assertEqual(True, False)  # add assertion here
 
 
