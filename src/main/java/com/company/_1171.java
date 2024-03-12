89956295936d6d9bc223f7949fe88f99f314eb5c
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2024-03-11 20:37:40 -0400

    1171 10/105 WA

---
+++ src/main/java/com/company/_1171.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import com.company.util.list.ListNode;
+
+public class _1171 {
+
+    /*Wrong Answer
+10 / 105 testcases passed*/
+    public ListNode removeZeroSumSublists(ListNode head) {
+
+    }
+}
