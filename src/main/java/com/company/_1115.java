7492c88a62511b3b3a001d8711fe41bbe31ac58f
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2024-03-12 22:41:41 -0400

    1115 96% synchronized lock

--- src/main/java/com/company/_1115.java
+++ src/main/java/com/company/_1115.java
@@ -2,8 +2,48 @@ package com.company;
 
 public class _1115 {
     private int n;
+
+    /*22
+ms
+Beats
+96.34%
+of users with Java*/
+    final Object lock;
+    boolean foo = true;
+
     public _1115 (int n) {
         this.n = n;
+        lock = new Object();
+    }
+
+
+    public void foo(Runnable printFoo) throws InterruptedException {
+
+        for (int i = 0; i < n; i++) {
+            synchronized (lock) {
+                while (!foo) {
+                    lock.wait();
+                }
+                // printFoo.run() outputs "foo". Do not change or remove this line.
+                printFoo.run();
+                foo = false;
+                lock.notifyAll();
+            }
+        }
+    }
+
+    public void bar(Runnable printBar) throws InterruptedException {
+        for (int i = 0; i < n; i++) {
+            synchronized (lock) {
+                while (foo) {
+                    lock.wait();
+                }
+                // printBar.run() outputs "bar". Do not change or remove this line.
+                printBar.run();
+                foo = true;
+                lock.notifyAll();
+            }
+        }
     }
 
     /*22
@@ -12,7 +52,7 @@ public class _1115 {
     96.34%
     of users with Java*/
 
-    boolean foo = true;
+    /*boolean foo = true;
 
     public synchronized void foo(Runnable printFoo) throws InterruptedException {
 
@@ -38,5 +78,5 @@ public class _1115 {
             foo = true;
             notifyAll();
         }
-    }
+    }*/
 }
