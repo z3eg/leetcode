96f541ff1e4a75b8868245c3482e9be6207b3312
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2024-03-15 00:58:27 -0400

    1115 96% reentrant lock

--- src/main/java/com/company/_1115.java
+++ src/main/java/com/company/_1115.java
@@ -7,13 +7,70 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class _1115 {
 
+    //condition variable
+    /*21
+    ms
+    Beats
+    99.29%
+    of users with Java*/
+    class FooBar {
+        private int n;
+        Lock lock;
+        Condition foo;
+        Condition bar;
+        boolean fooDone;
+        public FooBar (int n) {
+            lock = new ReentrantLock();
+            foo = lock.newCondition();
+            bar = lock.newCondition();
+            fooDone = false;
+            this.n = n;
+        }
+
+        public void foo(Runnable printFoo) throws InterruptedException {
+            for (int i = 0; i < n; i++) {
+                lock.lock();
+                try {
+                    while (fooDone) {
+                        foo.await();
+                    }
+                    // printFoo.run() outputs "foo". Do not change or remove this line.
+                    printFoo.run();
+                    fooDone = true;
+                    bar.signal();
+                }
+                finally {
+                    lock.unlock();
+                }
+            }
+        }
+
+        public void bar(Runnable printBar) throws InterruptedException {
+            for (int i = 0; i < n; i++) {
+                lock.lock();
+                try {
+                    while (!fooDone) {
+                        bar.await();
+                    }
+                    // printBar.run() outputs "bar". Do not change or remove this line.
+                    printBar.run();
+                    fooDone = false;
+                    foo.signal();
+                }
+                finally {
+                    lock.unlock();
+                }
+            }
+        }
+    }
+
     //semaphore
     /*22
     ms
     Beats
     96.46%
     of users with Java*/
-    class FooBar {
+    /*class FooBar {
         private int n;
         private Semaphore foo = new Semaphore(1);
         private Semaphore bar = new Semaphore(0);
@@ -38,7 +95,7 @@ public class _1115 {
                 foo.release();
             }
         }
-    }
+    }*/
 
 
 
