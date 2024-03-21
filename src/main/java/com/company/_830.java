ef1f00726844b26e9ee59e20250fabf8522d3cf3
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2024-03-21 08:16:30 -0400

    830 100%

--- src/main/java/com/company/_830.java
+++ src/main/java/com/company/_830.java
@@ -7,6 +7,32 @@ public class _830 {
 
     /*Wrong Answer
 137 / 202 testcases passed*/
+    /*public List<List<Integer>> largeGroupPositions(String s) {
+        List<List<Integer>> res = new LinkedList<>();
+        int streak = 1;
+        int streakStrart = 0;
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i)==s.charAt(i-1))
+                streak++;
+            else {
+                if (streak > 2) {
+                    List<Integer> list = new LinkedList<>();
+                    list.add(streakStrart);
+                    list.add(i-1);
+                    res.add(list);
+                }
+                streak = 1;
+                streakStrart = i;
+            }
+        }
+        return res;
+    }*/
+
+    /*1
+ms
+Beats
+100.00%
+of users with Java*/
     public List<List<Integer>> largeGroupPositions(String s) {
         List<List<Integer>> res = new LinkedList<>();
         int streak = 1;
@@ -24,6 +50,12 @@ public class _830 {
                 streak = 1;
                 streakStrart = i;
             }
+        }
+        if (streak > 2) {
+            List<Integer> list = new LinkedList<>();
+            list.add(streakStrart);
+            list.add(s.length()-1);
+            res.add(list);
         }
         return res;
     }
