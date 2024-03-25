68b4c79be541a13a9f75bca0fe8511a29e6b9885
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2024-03-24 20:43:30 -0400

    836 28/40

--- src/main/java/com/company/_836.java
+++ src/main/java/com/company/_836.java
@@ -6,8 +6,16 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class _836 {
+
+    /*Wrong Answer
+28 / 40 testcases passed*/
     public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
-        return true;
+        return inBetween(rec1[0], rec1[1], rec2[0], rec2[1], rec2[2], rec2[3]) ||
+                inBetween(rec1[2], rec1[3], rec2[0], rec2[1], rec2[2], rec2[3]);
+    }
+
+    boolean inBetween(int x, int y, int x1, int y1, int x2, int y2) {
+        return x > x1 && y > y1 && x < x2 && y < y2;
     }
 
     @Test
