220a39d18af3d5c7202c62469d436b41ec0543f7
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2024-03-25 20:58:01 -0400

    836 36/40

--- src/main/java/com/company/_836.java
+++ src/main/java/com/company/_836.java
@@ -43,7 +43,7 @@ public class _836 {
     }
 
     public boolean isRectangleOverlap(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
-        return (ax1 >= bx1 && ax1 <= bx2 || ax2 >= bx1 && ax2 >= bx2)
+        return (ax1 >= bx1 && ax1 <= bx2 || ax2 >= bx1 && ax2 >= bx2);
     }
 
     @Test
