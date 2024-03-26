c499c0735898e2e7798da88504a0efc4311e9949
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2024-03-25 21:03:54 -0400

    41 37%

--- src/main/java/com/company/_41.java
+++ src/main/java/com/company/_41.java
@@ -19,10 +19,28 @@ public class _41 {
 
     /*Wrong Answer
 175 / 176 testcases passed*/
-    public int firstMissingPositive(int[] nums) {
+    /*public int firstMissingPositive(int[] nums) {
         int[] freq = new int[100_001];
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] > 0 && nums[i]<100_001)
+                freq[nums[i]]++;
+        }
+        for (int i = 1; i < freq.length; i++) {
+            if (freq[i] == 0)
+                return i;
+        }
+        return 0;
+    }*/
+
+    /*3
+    ms
+    Beats
+    37.78%
+    of users with Java*/
+    public int firstMissingPositive(int[] nums) {
+        int[] freq = new int[100_002];
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0 && nums[i]<=100_001)
                 freq[nums[i]]++;
         }
         for (int i = 1; i < freq.length; i++) {
