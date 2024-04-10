8d4bff18553a2035030b899535cabcf4e0cd8af3
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2024-04-09 23:20:58 -0400

    2073 stub

--- src/main/java/com/company/_2073.java
+++ src/main/java/com/company/_2073.java
@@ -1,7 +1,17 @@
 package com.company;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class _2073 {
     public int timeRequiredToBuy(int[] tickets, int k) {
         return 0;
+    }
+
+    @Test
+    public void test() {
+        assertEquals(6, timeRequiredToBuy(new int[]{2,3,2}, 2));
+        assertEquals(8, timeRequiredToBuy(new int[]{5,1,1,1}, 0));
     }
 }
