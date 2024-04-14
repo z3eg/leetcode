ce6c740c8455cccf04e0aebc73ad0939406f76c6
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2024-04-14 18:08:50 -0400

    404 100%

--- src/main/java/com/company/_404.java
+++ src/main/java/com/company/_404.java
@@ -5,11 +5,29 @@ import com.company.util.tree.bst.TreeNode;
 public class _404 {
 
     public int sumOfLeftLeaves(TreeNode root) {
+
         return sum(root, 0);
     }
 
-//    29 / 100 testcases passed
+    /*0
+    ms
+    Beats
+    100.00%
+    of users with Java*/
     int sum(TreeNode root, int curSum) {
+        if (root!=null) {
+            if (root.left!=null && (root.left.left == null && root.left.right == null))
+                    {curSum+=root.left.val;
+            }
+            curSum = sum(root.left, curSum);
+            curSum = sum(root.right, curSum);
+        }
+        return curSum;
+    }
+
+
+//    29 / 100 testcases passed
+    /*int sum(TreeNode root, int curSum) {
         if (root!=null) {
             if (root.left!=null) {
                 curSum+=root.left.val;
@@ -18,5 +36,7 @@ public class _404 {
             curSum = sum(root.right, curSum);
         }
         return curSum;
-    }
+    }*/
+
+
 }
