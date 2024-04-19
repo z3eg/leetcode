4531f65e2c2e9d218ccce45314ef6839d4a236a0
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2024-04-19 13:42:40 -0400

    200 86%

--- src/main/java/com/company/_200.java
+++ src/main/java/com/company/_200.java
@@ -2,29 +2,40 @@ package com.company;
 
 public class _200 {
 
+    /*3
+    ms
+    Beats
+    86.63%
+    of users with Java*/
     public int numIslands(char[][] grid) {
         boolean[][] marked = new boolean[grid.length][grid[0].length];
         int counter = 0;
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[i].length; j++) {
-                if (!marked[i][j]) {
-                    counter = markAdjacent(grid, marked, i, j, counter);
+                if (!marked[i][j] && grid[i][j]=='1') {
+                    counter++;
+                    markAdjacent(grid, marked, i, j);
                 }
             }
         }
         return counter;
     }
 
-    private int markAdjacent(char[][] grid, boolean[][] marked, int i, int j, int counter) {
-        if (i>=0 && i < grid.length && j >=0 && j < grid[i].length) {
+    private void markAdjacent(char[][] grid, boolean[][] marked, int i, int j) {
+        if (!marked[i][j] && grid[i][j]=='1') {
             marked[i][j]=true;
-            if (i-1>=0 && grid[i-1][j]!=0 && !marked[i-1][j]) {
-                counter = markAdjacent(grid, marked, i-1, j, counter);
+            if (i-1>=0 && grid[i-1][j]!='0' && !marked[i-1][j]) {
+                markAdjacent(grid, marked, i-1, j);
             }
-            if (i+1<grid.length && grid[i+1][j]!=0 && !marked[i+1][j]) {
-                counter = markAdjacent(grid, marked, i+1, j, counter);
+            if (i+1<grid.length && grid[i+1][j]!='0' && !marked[i+1][j]) {
+                markAdjacent(grid, marked, i+1, j);
+            }
+            if (j+1<grid[i].length && grid[i][j+1]!='0' && !marked[i][j+1]) {
+                markAdjacent(grid, marked, i, j+1);
+            }
+            if (j-1>=0 && grid[i][j-1]!='0' && !marked[i][j-1]) {
+                markAdjacent(grid, marked, i, j-1);
             }
         }
-        return counter;
     }
 }
