19340de54547d58fb243343fd5a8ecf160c6d950
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2024-04-21 13:40:51 -0400

    1971 22/30 MLE

--- src/main/java/com/company/_1971.java
+++ src/main/java/com/company/_1971.java
@@ -1,7 +1,33 @@
 package com.company;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class _1971 {
+
+    /*Memory Limit Exceeded
+    22 / 30 testcases passed*/
     public boolean validPath(int n, int[][] edges, int source, int destination) {
-        return true;
+        boolean[] visited = new boolean[n];
+        boolean[][] paths = new boolean[n][n];
+        for (int[] edge : edges) {
+            paths[edge[0]][edge[1]] = true;
+            paths[edge[1]][edge[0]] = true;
+        }
+        Queue<Integer> q = new LinkedList<>();
+        q.add(source);
+        while (!q.isEmpty()) {
+            Integer poll = q.poll();
+            if (poll == destination)
+                return true;
+            for (int i = 0; i < n; i++) {
+                if (!visited[i] && paths[i][poll]) {
+                    visited[i] = true;
+                    q.add(i);
+                }
+            }
+        }
+        return false;
     }
 }
