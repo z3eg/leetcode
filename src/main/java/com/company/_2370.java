562a7a6074dbb45d057f9ea565eb9ec3c81815f7
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2024-04-24 20:52:52 -0400

    2370 stub

---
+++ src/main/java/com/company/_2370.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class _2370 {
+    public int longestIdealString(String s, int k) {
+        return 0;
+    }
+
+    @Test
+    public void test() {
+        assertEquals(4, longestIdealString("acfgbd", 2));
+        assertEquals(4, longestIdealString("abcd", 3));
+    }
+}
