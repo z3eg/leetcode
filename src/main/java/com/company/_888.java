d79c4eae03e44f40a76d81331022f529d845b75c
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2024-04-27 17:19:56 -0400

    888 24%

---
+++ src/main/java/com/company/_888.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import org.junit.Test;
+
+public class _888 {
+
+    /*204
+    ms
+    Beats
+    24.28%
+    of users with Java*/
+    public int[] fairCandySwap(int[] aliceSizes, int[] bobSizes) {
+        int aTot = 0;
+        int bTot = 0;
+        for (int a : aliceSizes)
+            aTot+= a;
+        for (int b : bobSizes)
+            bTot += b;
+        int avg = (aTot+bTot)/2;
+        for (int a : aliceSizes) {
+            for (int b : bobSizes) {
+                if (aTot-a+b==avg)
+                    return new int[]{a,b};
+            }
+        }
+        return null;
+    }
+
+    @Test
+    public void test() {
+
+    }
+}
