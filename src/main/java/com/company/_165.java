6962353ef217fe8586b476e5ed993c08f10463cb
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2024-05-03 00:26:20 -0400

    165 22/84 WA

---
+++ src/main/java/com/company/_165.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class _165 {
+
+    /*Wrong Answer
+22 / 84 testcases passed*/
+    public int compareVersion(String version1, String version2) {
+        return 0;
+    }
+
+}
