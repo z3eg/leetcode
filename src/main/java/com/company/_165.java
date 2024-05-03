a8182d25167045e01069df53c7158a80cbd58dd7
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2024-05-03 18:14:59 -0400

    165 69/84 WA

--- src/main/java/com/company/_165.java
+++ src/main/java/com/company/_165.java
@@ -1,11 +1,31 @@
 package com.company;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class _165 {
 
     /*Wrong Answer
-22 / 84 testcases passed*/
+69 / 84 testcases passed*/
     public int compareVersion(String version1, String version2) {
+        String[] s1 = version1.split("\\.");
+        String[] s2 = version2.split("\\.");
+        int minlen = Math.min(s1.length, s2.length);
+        for (int i = 0; i < minlen; i++) {
+            if (Integer.parseInt(s1[i]) < Integer.parseInt(s2[i]))
+                return -1;
+            if (Integer.parseInt(s1[i]) > Integer.parseInt(s2[i]))
+                return 1;
+        }
         return 0;
+    }
+
+    @Test
+    public void test() {
+        assertEquals(-1, compareVersion("0.1","1.1"));
+        assertEquals(0, compareVersion("1.01","1.001"));
+        assertEquals(0, compareVersion("1.0","1.0.0"));
     }
 
 }
