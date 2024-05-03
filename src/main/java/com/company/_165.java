7315c62ce996688555e9015293bed0e0d4028af2
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2024-05-03 18:20:18 -0400

    165 78%

--- src/main/java/com/company/_165.java
+++ src/main/java/com/company/_165.java
@@ -8,6 +8,24 @@ public class _165 {
 
     /*Wrong Answer
 69 / 84 testcases passed*/
+    /*public int compareVersion(String version1, String version2) {
+        String[] s1 = version1.split("\\.");
+        String[] s2 = version2.split("\\.");
+        int minlen = Math.min(s1.length, s2.length);
+        for (int i = 0; i < minlen; i++) {
+            if (Integer.parseInt(s1[i]) < Integer.parseInt(s2[i]))
+                return -1;
+            if (Integer.parseInt(s1[i]) > Integer.parseInt(s2[i]))
+                return 1;
+        }
+        return 0;
+    }*/
+
+    /*1ms
+    Beats
+    78.70%
+    of users with Java
+    */
     public int compareVersion(String version1, String version2) {
         String[] s1 = version1.split("\\.");
         String[] s2 = version2.split("\\.");
@@ -17,6 +35,20 @@ public class _165 {
                 return -1;
             if (Integer.parseInt(s1[i]) > Integer.parseInt(s2[i]))
                 return 1;
+        }
+        String[] s;
+        int ans;
+        if (s1.length > s2.length) {
+            s = s1;
+            ans = 1;
+        }
+        else {
+            s = s2;
+            ans = -1;
+        }
+        for (int i = minlen; i < s.length; i++) {
+            if (Integer.parseInt(s[i]) > 0)
+                return ans;
         }
         return 0;
     }
