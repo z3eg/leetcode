e42afb59db249254a17648d1e136be7da47d7125
zeeg <zeeglynch@gmail.com>
2024-05-08 21:31:37 -0400

    3075 97%

--- src/main/java/com/company/_3075.java
+++ src/main/java/com/company/_3075.java
@@ -10,9 +10,32 @@ public class _3075 {
 
     /*Wrong Answer
 660 / 674 testcases passed*/
-    public long maximumHappinessSum(int[] happiness, int k) {
+    /*public long maximumHappinessSum(int[] happiness, int k) {
         Arrays.sort(happiness);
         int res = 0;
+        int rounds = 0;
+        int totReduced = 0;
+        while (k>0) {
+            rounds++;
+            int cur = happiness[happiness.length - rounds];
+            cur-=totReduced;
+            if (cur > 0) {
+                res+=cur;
+            }
+            k--;
+            totReduced++;
+        }
+        return res;
+    }*/
+
+    /*34
+    ms
+    Beats
+    97.44%
+    of users with Java*/
+    public long maximumHappinessSum(int[] happiness, int k) {
+        Arrays.sort(happiness);
+        long res = 0;
         int rounds = 0;
         int totReduced = 0;
         while (k>0) {
