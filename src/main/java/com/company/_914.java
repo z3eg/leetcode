2a27811f811938ebdd83550e22c6c3ad5dfa455b
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2024-05-10 22:33:59 -0400

    914 46/75 WA

---
+++ src/main/java/com/company/_914.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class _914 {
+
+    /*Wrong Answer
+46 / 75 testcases passed*/
+    public boolean hasGroupsSizeX(int[] deck) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int c : deck) {
+            Integer am = map.get(c);
+            if (am == null)
+                am = 0;
+            am++;
+            map.put(c, am);
+        }
+        Iterator<Map.Entry<Integer, Integer>> it = map.entrySet().iterator();
+        int fp = it.next().getValue();
+        while (it.hasNext()) {
+            Map.Entry<Integer, Integer> next = it.next();
+            if (next.getValue() != fp)
+                return false;
+        }
+        return true;
+    }
+}
