1441fca96b29673ed324ea6263b19376da156a20
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2024-05-10 23:17:19 -0400

    917 100%

--- src/main/java/com/company/_917.java
+++ src/main/java/com/company/_917.java
@@ -3,7 +3,7 @@ package com.company;
 public class _917 {
     /*
 Runtime Error 5 / 115 testcases passed */
-    public String reverseOnlyLetters(String s) {
+    /*public String reverseOnlyLetters(String s) {
         char[] chars = s.toCharArray();
         int l = 0;
         int r = s.length()-1;
@@ -11,6 +11,32 @@ Runtime Error 5 / 115 testcases passed */
             while (!isLetter(chars[l]))
                 l++;
             while (!isLetter(chars[r]))
+                r--;
+            if (l < r) {
+                char tmp = chars[l];
+                chars[l] = chars[r];
+                chars[r] = tmp;
+            }
+            l++;
+            r--;
+        }
+        return new String(chars);
+    }*/
+
+    /*Runtime
+    0
+    ms
+    Beats
+    100.00%
+    of users with Java*/
+    public String reverseOnlyLetters(String s) {
+        char[] chars = s.toCharArray();
+        int l = 0;
+        int r = s.length()-1;
+        while (l < r) {
+            while (l < chars.length && !isLetter(chars[l]))
+                l++;
+            while (r >= 0 && !isLetter(chars[r]))
                 r--;
             if (l < r) {
                 char tmp = chars[l];
