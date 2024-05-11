0e5e471b413c39d118d5855f1bad3fdbef42673c
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2024-05-10 22:35:48 -0400

    914 47/75 WA

--- src/main/java/com/company/_914.java
+++ src/main/java/com/company/_914.java
@@ -8,6 +8,28 @@ public class _914 {
 
     /*Wrong Answer
 46 / 75 testcases passed*/
+    /*public boolean hasGroupsSizeX(int[] deck) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int c : deck) {
+            Integer am = map.get(c);
+            if (am == null)
+                am = 0;
+            am++;
+            map.put(c, am);
+        }
+        Iterator<Map.Entry<Integer, Integer>> it = map.entrySet().iterator();
+        int fp = it.next().getValue();
+        while (it.hasNext()) {
+            Map.Entry<Integer, Integer> next = it.next();
+            if (next.getValue() != fp)
+                return false;
+        }
+        return true;
+    }*/
+
+
+    /*Wrong Answer
+47 / 75 testcases passed*/
     public boolean hasGroupsSizeX(int[] deck) {
         Map<Integer, Integer> map = new HashMap<>();
         for (int c : deck) {
@@ -19,6 +41,8 @@ public class _914 {
         }
         Iterator<Map.Entry<Integer, Integer>> it = map.entrySet().iterator();
         int fp = it.next().getValue();
+        if (fp < 2)
+            return false;
         while (it.hasNext()) {
             Map.Entry<Integer, Integer> next = it.next();
             if (next.getValue() != fp)
