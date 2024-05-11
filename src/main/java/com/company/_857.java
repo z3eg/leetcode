8af3680319ddcedba93df9d48d91a0435b722f9e
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2024-05-10 22:19:41 -0400

    857 stub

---
+++ src/main/java/com/company/_857.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class _857 {
+    public double mincostToHireWorkers(int[] quality, int[] wage, int k) {
+        return 0;
+    }
+
+    @Test
+    public void test() {
+        assertEquals(105, mincostToHireWorkers(new int[]{10,20,5}, new int[]{70,50,30}, 2));
+        assertEquals(30.66667, mincostToHireWorkers(new int[]{3,1,10,10,1}, new int[]{4,8,2,2,7}, 3));
+    }
+}
