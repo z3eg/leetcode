4f4528972f86c36c7dc7aa100296b173d01d92c6
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2024-05-22 21:19:49 -0400

    2597 stub

---
+++ src/main/java/com/company/_2597.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class _2597 {
+    public int beautifulSubsets(int[] nums, int k) {
+        return 0;
+    }
+
+    @Test
+    public void test() {
+        assertEquals(4, beautifulSubsets(new int[]{2,4,6},2));
+        assertEquals(1, beautifulSubsets(new int[]{1},1));
+    }
+}
