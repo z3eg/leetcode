4c314780e3671da3488564afe54ac93b46965dd9
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2024-05-26 11:10:40 -0400

    552 stub

---
+++ src/main/java/com/company/_552.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class _552 {
+
+    public int checkRecord(int n) {
+        return 0;
+    }
+
+    @Test
+    public void test() {
+        assertEquals(8, checkRecord(2));
+        assertEquals(3, checkRecord(1));
+        assertEquals(183236316, checkRecord(10101));
+    }
+}
