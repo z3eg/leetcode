39343d336c4c4846fd84f369807610a0f187fddb
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2024-05-28 18:59:29 -0400

    1208 10/37

--- src/main/java/com/company/_1208.java
+++ src/main/java/com/company/_1208.java
@@ -5,9 +5,32 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class _1208 {
+
+    /*Wrong Answer
+10 / 37 testcases passed*/
     public int equalSubstring(String s, String t, int maxCost) {
         //populate the array of diffs and do a dynamic sliding window over it.
-        return 0;
+        int[] diffs = new int[s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            diffs[i] = Math.abs(s.charAt(i)-t.charAt(i));
+        }
+        int maxLen = 0;
+        int curCost = 0;
+        int l = 0;
+        int r = 0;
+        while (r < s.length()-1) {
+            while (curCost <= maxCost && r < s.length()-1) {
+                curCost+=diffs[r];
+                if (curCost <= maxCost)
+                    maxLen = Math.max(maxLen, r-l+1);
+                r++;
+            }
+            while (l < s.length()-1 && (l==r || (curCost > maxCost && l<r))) {
+                l++;
+                curCost-=diffs[l];
+            }
+        }
+        return maxLen;
     }
 
     @Test
