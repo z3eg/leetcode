91047879510467c6df516cd7209710d55fcff151
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2024-05-29 20:11:35 -0400

    976 stub

---
+++ src/main/java/com/company/_976.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import org.junit.Test;
+
+public class _976 {
+
+    public int largestPerimeter(int[] nums) {
+        return 0;
+    }
+
+    @Test
+    public void test() {
+
+    }
+}
