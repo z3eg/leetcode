84eeb3a37b86f02df40083e53764b302afcfe7b6
Dmytro Cherednyk <dmytro.cherednyk-contractor@procore.com>
2024-05-29 20:06:20 -0400

    1442 stub

---
+++ src/main/java/com/company/_1442.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class _1442 {
+    public int countTriplets(int[] arr) {
+        return 0;
+    }
+
+    @Test
+    public void test() {
+        assertEquals(4, countTriplets(new int[]{2,3,1,6,7}));
+        assertEquals(10, countTriplets(new int[]{1,1,1,1,1}));
+    }
+}
